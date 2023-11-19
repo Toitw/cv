d2bdf0842eae3b9f0606c5574ac1d02e8aeae7a2
juroga_new <juanjrgast@gmail.com>
2023-11-19 16:58:01 +0100

    Created first draf GeneralInfo module

---
+++ src/components/GeneralInfo.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import '../styles/GeneralInfo.css';
+
+const FieldOrText = ({ label, name, type, value, onChange, editing }) => {
+    return (
+        <label>
+            {label}:
+            {editing ? (
+                <input 
+                    type={type}
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                />
+            ) : (
+                <span> {value}</span>
+            )}
+        </label>
+    );
+};
+
+function GeneralInfo() {
+    const [editing, setEditing] = useState(false);
+    const [info, setInfo] = useState({ name: '', surname: '', email: '', telephone: '' });
+
+    const handleInputChange = (e) => {
+        setInfo({ ...info, [e.target.name]: e.target.value });
+    };
+
+    const toggleEdit = () => {
+        setEditing(!editing);
+    };
+
+    return (
+        <div>
+            <form className='general-info-form'>
+                <FieldOrText label="Name" name="name" type="text" value={info.name} onChange={handleInputChange} editing={editing} />
+                <FieldOrText label="Surname" name="surname" type="text" value={info.surname} onChange={handleInputChange} editing={editing} />
+                <FieldOrText label="Email" name="email" type="email" value={info.email} onChange={handleInputChange} editing={editing} />
+                <FieldOrText label="Telephone" name="telephone" type="tel" value={info.telephone} onChange={handleInputChange} editing={editing} />
+                <button type="button" onClick={toggleEdit}>{editing ? 'Save' : 'Edit'}</button>
+            </form>
+        </div>
+    );
+}
+
+export default GeneralInfo;
+
+
